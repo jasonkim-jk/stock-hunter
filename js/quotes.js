a47577f8a9bb55660bc85220f4654dab2cb899e1
Jason Kim <thehesedlife@gmail.com>
2020-06-24 11:39:20 -0700

    added animation on random quotes

--- js/quotes.js
+++ js/quotes.js
@@ -2,8 +2,23 @@
 function updateQuote() {
   $.getJSON(urlGetQuote, (data) => {
     quote.textContent = data.content;
-    author.textContent = data.author;
+    author.textContent = "— " + data.author;
+    addAnimationEffect(quote, 3);
+    addAnimationEffect(author, 3);
   }).fail((jqxhr, textStatus, error) => {
     console.error(textStatus + ", " + error);
   });
+}
+
+function addAnimationEffect(element, delaySec) {
+  let newDom = "";
+
+  for (let i = 0; i < element.innerText.length; i++) {
+    newDom += '<span class="char">' + (element.innerText[i] == " " ? "&nbsp;" : element.innerText[i]) + "</span>";
+  }
+  element.innerHTML = newDom;
+
+  for (let i = 0; i < element.children.length; i++) {
+    element.children[i].style["animation-delay"] = delaySec * i + "ms";
+  }
 }
